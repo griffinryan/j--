ac36dd69ac9224669ef75fdd1a2532d2eaf8036c
griffinryan <glryan@uw.edu>
2024-01-22 20:11:26 -0800

    inital j-- commit

---
+++ tests/jvm/HelloWorld.java
@@ -0,0 +1,14 @@
+// Copyright 2012- Bill Campbell, Swami Iyer and Bahar Akbal-Delibas
+//
+// Writes to standard output the message "Hello, World".
+
+import java.lang.System;
+
+public class HelloWorld {
+    // Entry point.
+    //*
+    // testing multi-line comments. */
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
